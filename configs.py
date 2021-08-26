1281a13cb6c3215d5c5942974dd862bbbef2be69
Avishkar Patil <avishkarpatil29@gmail.com>
2021-08-26 20:13:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -24,26 +24,18 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
-📝 **Language:** [Python3](https://www.python.org)
+🧑🏻‍💻 **Developer:** @AvishkarPatil
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+👥 **Follow GitHub:** [AviPatilPro](https://GitHub.com/avipatilpro)
 
-📡 **Hosted on:** [Heroku](https://heroku.com)
-
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
-
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Movies Here:** [MoviesHubFlix](https://t.me/MoviesHubFlix)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @AvishkarPatil
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
