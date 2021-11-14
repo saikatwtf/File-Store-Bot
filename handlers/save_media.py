de5ac43f9815f3240949092c2d6adb967e176a77
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2021-11-14 18:51:37 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -46,7 +46,7 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=_MoviZenX_{str_to_b64(str(SaveMessage.message_id))}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=MoviZenX_{str_to_b64(str(SaveMessage.message_id))}"
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
             f"Just Click the link to get your files!",
@@ -84,7 +84,7 @@ async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             parse_mode="Markdown", disable_web_page_preview=True)
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=_MoviZenX_{str_to_b64(file_er_id)}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=MoviZenX_{str_to_b64(file_er_id)}"
         await editable.edit(
             f"**Your File Stored in my Database!**\n\nHere is the Permanent Link of your file: {share_link} \n\n"
             f"Just Click the link to get your file!",
